45ce7a11980077c480db5891b046936a6380199b
Max Kozlov <dreamx.max@gmail.com>
2017-03-23 18:51:39 +0300

    git st

--- ignite/project.clj
+++ ignite/project.clj
@@ -6,4 +6,8 @@
   :main jepsen.ignite
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [jepsen "0.1.4"]
-                 [avout "0.5.4" :exclusions [org.slf4j/slf4j-log4j12]]])
+                 [javax.cache/cache-api "1.0.0"]
+                 [org.apache.ignite/ignite-core "1.9.0"]
+                 [org.apache.ignite/ignite-log4j "1.9.0"]
+                 [org.apache.ignite/ignite-indexing "1.9.0"]
+                 [org.apache.ignite/ignite-spring "1.9.0"]])
--- ignite/src/jepsen/ignite.clj
+++ ignite/src/jepsen/ignite.clj
@@ -21,7 +21,16 @@
             [jepsen.control [net :as net]
                             [util :as net/util]]
             [jepsen.os.debian :as debian]
-            [knossos.core :as knossos]))
+            [knossos.core :as knossos])
+  (:import (org.apache.ignite Ignition
+                              Ignite
+                              IgniteCluster)
+            (org.apache.ignite.cluster ClusterGroup)
+            (org.apache.ignite.lang IgniteRunnable)
+            (org.apache.ignite.configuration CacheConfiguration)
+            (org.apache.ignite.cache CachePeekMode)
+            (org.apache.ignite.configuration IgniteConfiguration)
+            (java.lang Thread)))
 
 (defn install!
   "Installs ignite."
@@ -31,15 +40,14 @@
       (info node "download ignite" version)
       (c/cd "/tmp"
             (c/exec :wget (str "http://apache-mirror.rbc.ru/pub/apache//ignite/" version "/apache-ignite-fabric-" version "-bin.zip"))
-            (c/exec :unzip "apache-ignite-fabric-1.8.0-bin.zip"))
-      (c/cd (str "/tmp/apache-ignite-fabric-" version "-bin")
-            (c/exec (str "./bin/ignite.sh config/default-config.xml")))))))
+            (c/exec :unzip (str "apache-ignite-fabric-" version "-bin.zip"))
+      create startup script
+      (c/exec :echo (str 
+              "#!/bin/bash\nfaketime -m -f \"+$((RANDOM%100))s x1.${RANDOM}\" /tmp/apache-ignite-fabric-" version "-bin/bin/ignite.sh config/default-config.xml") :> "/usr/bin/ignited")
+      (c/exec :chmod "0755" "/usr/bin/ignited")
+      (c/exec :ignited)))))
+)
 
-      ; ; Replace /usr/bin/asd with a wrapper that skews time a bit
-      ; (c/exec :mv   "/usr/bin/asd" "/usr/local/bin/asd")
-      ; (c/exec :echo
-      ;         "#!/bin/bash\nfaketime -m -f \"+$((RANDOM%100))s x1.${RANDOM}\" /usr/local/bin/asd" :> "/usr/bin/asd")
-      ; (c/exec :chmod "0755" "/usr/bin/asd"))))
 
 (defn db [version]
   "Ignite for a particular version."
@@ -49,11 +57,84 @@
         (install! version)))
     (teardown! [_ test node])))
 
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
+(defn start-client
+  "Start client node."
+  [node]
+  (Ignition/setClientMode true)
+  (let [gridname (str "Client" (clojure.string/replace node ":" ""))
+        cfg (new IgniteConfiguration)]
+        (.setGridName cfg gridname)
+        (.setPeerClassLoadingEnabled cfg true)
+        (Ignition/start cfg)))
+
+(defn ignite-cas! [cache key value new-value]
+  (let [existing-value (:value (.get cache key))]
+    ; (println "++++++++")
+    ; (println existing-value)
+    ; (println value)
+    ; (println "++++++++")
+    (if (= existing-value value)
+        (.putIfAbsent cache key new-value)
+        false)))
+
+(defrecord CasRegisterClient [ignite cache namecache key]
+  client/Client
+  (setup! [this test node]
+    (let [ignite (start-client node)
+          cache (.getOrCreateCache ignite (new CacheConfiguration namecache))]
+      (assoc this :cache cache, :ignite ignite)))
+
+  (invoke! [this test op]
+      (case (:f op)
+        :read (assoc op
+                     :type :ok,
+                     :value (.get cache key))
+
+        :cas   (let [[value value'] (:value op)
+                     ok?            (ignite-cas! cache
+                                               key
+                                               value
+                                               value')]
+               (assoc op :type (if ok? :ok :fail)))
+
+        :write (do (.put cache key (:value op))
+                   (assoc op :type :ok))))
+
+  (teardown! [this test]
+    (.close ignite)))
+
+(defn cas-register-client
+  "A basic CAS register on top of a single key."
+  []
+  (CasRegisterClient. nil nil "jepsen" "mew"))
+
 (defn ignite-test
   "Given an options map from the command-line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
-  (merge tests/noop-test {:db (db "1.8.0")}))
+  (merge tests/noop-test 
+          { :name    "ignite"
+            :os      debian/os
+            :db      (db "1.9.0")
+            :client  (cas-register-client)
+            :nemesis (nemesis/partition-random-halves)
+            :generator (->> (gen/mix [r w cas])
+                            (gen/stagger 1/10)
+                            (gen/nemesis
+                              (gen/seq (cycle [(gen/sleep 5)
+                                               {:type :info, :f :start}
+                                               (gen/sleep 5)
+                                               {:type :info, :f :stop}])))
+                            (gen/time-limit 60))
+            :model   (model/cas-register 0)
+            :checker (checker/compose
+                    {:perf   (checker/perf)
+                     :linear checker/linearizable})}
+         opts))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
